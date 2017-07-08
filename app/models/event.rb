588076163002ec18dc30db569dd406e075034e5a
lihantao <htl00001111@163.com>
2017-07-08 11:30:43 +0800

    finish rails 10-2

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ApplicationRecord
 
+  has_many :tickets, :dependent => :destroy
+  accepts_nested_attributes_for :tickets, :allow_destroy => true, :reject_if => :all_blank
   belongs_to :category, :optional => true
 
   STATUS = ["draft", "public", "private"]
